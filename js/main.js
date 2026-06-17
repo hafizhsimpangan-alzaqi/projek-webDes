0698b7f67409b4fc7a035d3027178b5b8c0770cd
hafizhsimpangan-alzaqi <hafizhalzaqi@gmail.com>
2026-06-17 23:47:15 +0700

    selesai membuat contact us, about us walaupun nnati ada sedkit perbaikan dan template untuk courses

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@ const sidebar = document.getElementById("sidebar")
 function toggleSidebar() {
     sidebar.classList.toggle('show-toggle')
 }
+
+
+function goToHome(){
+    window.location.href = "../html/landingpage.html"
+}
+function goToAbout(){
+    window.location.href = "../html/about.html"
+}
+function goToCourse(){
+    window.location.href = "../html/courses.html"
+}
+function goToContact(){
+    window.location.href = "../html/contact.html"
+}
